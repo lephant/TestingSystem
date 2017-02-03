51c09edf3fe1fee8be6fd4d0c4a621c4ae0c0b05
lephant <levb97@yandex.ru>
2017-02-03 20:58:29 +0300

    now groups are loaded before showing

--- src/main/java/ru/lephant/java/rgatu/TestingSystem/controllers/GroupWindowController.java
+++ src/main/java/ru/lephant/java/rgatu/TestingSystem/controllers/GroupWindowController.java
@@ -1,11 +1,48 @@
 package ru.lephant.java.rgatu.TestingSystem.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+import org.hibernate.Session;
+import ru.lephant.java.rgatu.TestingSystem.entities.Group;
+import ru.lephant.java.rgatu.TestingSystem.hibernate.HibernateUtil;
 
-public class GroupWindowController {
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class GroupWindowController implements Initializable {
+
+    @FXML
+    private ListView<Group> groupListView;
+    private ObservableList<Group> groups = FXCollections.observableArrayList();
 
     private Stage mainStage;
     private Stage modalStage;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        groupListView.setItems(groups);
+        initGroups();
+    }
+
+    @SuppressWarnings("unchecked")
+    private void initGroups() {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            List<Group> list = session.createCriteria(Group.class).list();
+            groups.setAll(list);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
 
 
     public void setMainStage(Stage mainStage) {
