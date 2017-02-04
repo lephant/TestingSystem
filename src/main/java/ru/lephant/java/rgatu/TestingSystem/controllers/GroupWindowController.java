4809bdfe7523fdc9017728892d2a1d10931cfe0f
lephant <levb97@yandex.ru>
2017-02-04 11:14:19 +0300

    Added context menu to list view and fixed bug

--- src/main/java/ru/lephant/java/rgatu/TestingSystem/controllers/GroupWindowController.java
+++ src/main/java/ru/lephant/java/rgatu/TestingSystem/controllers/GroupWindowController.java
@@ -8,6 +8,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -18,6 +19,7 @@ import ru.lephant.java.rgatu.TestingSystem.hibernate.HibernateUtil;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class GroupWindowController implements Initializable {
@@ -50,11 +52,11 @@ public class GroupWindowController implements Initializable {
     @FXML
     public void onEditButtonClicked() {
         int index = groupListView.getSelectionModel().getSelectedIndex();
-        Group group = groups.get(index);
-        if (group == null) {
-            showNoSelectedGroupAlert();
+        if (index < 0) {
+            showNoSelectedGroupAlert("Не выбрана группа для редактирования!");
             return;
         }
+        Group group = groups.get(index);
         boolean needToSave = showGroupChangingDialog("Редактирование группы", group);
         if (needToSave) {
             saveGroup(group);
@@ -62,10 +64,10 @@ public class GroupWindowController implements Initializable {
         }
     }
 
-    private void showNoSelectedGroupAlert() {
+    private void showNoSelectedGroupAlert(String headerText) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Ошибка!");
-        alert.setHeaderText("Не выбрана группа для редактирования!");
+        alert.setHeaderText(headerText);
         alert.setContentText(null);
         alert.show();
     }
@@ -73,8 +75,23 @@ public class GroupWindowController implements Initializable {
     @FXML
     public void onDeleteButtonClicked() {
         Group group = groupListView.getSelectionModel().getSelectedItem();
-        deleteGroup(group);
-        groups.remove(group);
+        if (group != null) {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Подтверждение");
+            alert.setHeaderText("Вы уверены, что хотите удалить выбранную группу?");
+            alert.setContentText(null);
+
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK){
+                deleteGroup(group);
+                groups.remove(group);
+                alert.close();
+            } else {
+                alert.close();
+            }
+        } else {
+            showNoSelectedGroupAlert("Не выбрана группа для удаления!");
+        }
     }
 
 
